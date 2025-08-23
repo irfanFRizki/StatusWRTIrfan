11340cf6bf758f355d8bc6ccfc8941cf2a469055
irfanFRizki <160555692+irfanFRizki@users.noreply.github.com>
2025-08-23 14:16:57 +0700

    Delete usr/lib/lua/luci/controller/status_monitor.lua

--- usr/lib/lua/luci/controller/status_monitor.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.status_monitor", package.seeall)
-
-function index()
-    entry({"admin", "services", "status_monitor"}, template("status_monitor"), _("Status Monitor"), 100).dependent=false
-end
