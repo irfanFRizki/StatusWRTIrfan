a3219205c9d1ae7f8a3a186e4517efa906908f5a
irfanFRizki <160555692+irfanFRizki@users.noreply.github.com>
2026-03-12 09:38:09 +0700

    Delete scripts directory

--- scripts/changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION=$1
-
-echo "## v$VERSION - $(date +%Y-%m-%d)" >> CHANGELOG.md
-git log -5 --pretty=format:"- %s" >> CHANGELOG.md
-echo "" >> CHANGELOG.md
-echo "" >> CHANGELOG.md
